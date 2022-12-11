b025f3d5ad42148f188b4950737bfc3016fdd1fa
root <root@DESKTOP-ND99E9V.localdomain>
2022-12-10 20:45:27 -1000

    testcommit2

--- dj/ccdj/ccdj/settings.py
+++ dj/ccdj/ccdj/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'ccdj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': '5432'
     }
 }
+
+
 
 
 # Password validation
