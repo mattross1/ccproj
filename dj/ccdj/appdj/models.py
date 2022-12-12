15322978b6b7330d932daaca54e5008b015bd946
John Smith <email@example.com>
2022-12-12 00:27:55 -0600

    swap1

--- dj/ccdj/appdj/models.py
+++ dj/ccdj/appdj/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Foods(models.Model):
+    entry = models.IntegerField()
+    first = models.CharField(max_length=30)
+    last = models.CharField(max_length=30)
+    favorite = models.CharField(max_length=50)
+
--- dj/ccdj/ccdj/settings.py
+++ dj/ccdj/ccdj/settings.py
@@ -73,6 +73,7 @@ WSGI_APPLICATION = 'ccdj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -83,7 +84,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
+"""
 
 
 
